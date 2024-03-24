42484e5751c8d81b7d0b5c392479fb491b6eb3bc
waltersimekha <waltersimekha@gmail.com>
2024-03-24 23:36:54 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 4, 'M', 'brown', 'docile', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 5, 'M', 'black', 'mean', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 6, 'F', 'gray', 'loving', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 7, 'M', 'brown', 'asshole',False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 8, 'F', 'black', 'caring', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 9, 'M', 'brown', 'grumpy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 10, 'F', 'gray', 'docile', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 11, 'F', 'brown', 'docile', True);
